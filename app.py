6782db9071b75b34eebd7dc1aaeb61735f46482a
simks77 <sim_ks77@yahoo.com>
2025-07-15 21:32:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-import utils
-utils.greeting("kssim")
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, YOURNAME!</p>"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
